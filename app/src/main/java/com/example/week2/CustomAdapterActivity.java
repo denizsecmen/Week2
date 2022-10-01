a0ea6c0d8ef25056f353399ed774b93da1282eed
Deniz Seçmen <denizsecmen1240@gmail.com>
2022-10-02 02:01:14 +0300

    asasdass

---
+++ app/src/main/java/com/example/week2/CustomAdapterActivity.java
@@ -0,0 +1,19 @@
+package com.example.week2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.example.week2.databinding.ActivityCustomAdapterBinding;
+
+public class CustomAdapterActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
